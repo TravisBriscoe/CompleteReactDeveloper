84792f8db16f4ee8ed49b5e7305d9bbc86336b78
AnomicAphasia <travis.briscoe@gmail.com>
2021-07-29 09:47:56 -0400

    integrated signup with email/password

--- crwn-clothing/src/firebase/firebase.utils.jsx
+++ crwn-clothing/src/firebase/firebase.utils.jsx
@@ -14,6 +14,33 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, displayName, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const shot = await userRef.get();
+
+  if (!shot.exists) {
+    const { email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (err) {
+      console.log('error creating user', err.message);
+    }
+
+  }
+  
+  return userRef;
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
